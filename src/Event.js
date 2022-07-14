1c4d618573b4f6486fdcef3507b23185f8c5568d
Kirsten <kirsten.netter@gmx.de>
2022-07-14 07:59:17 +0200

    ex4.4 some styling

--- src/Event.js
+++ src/Event.js
@@ -23,11 +23,15 @@ class Event extends Component {
         return (
             <div className="event">
                 <h3 className="title">{event.summary}</h3>
-                <p className="start-time">
-                    {event.start.dateTime.slice(0,10)}                  
-                </p>
-                 
-                <p className="location">{event.location}</p>  
+                <div className="fb-date-location">
+                    <p className="start-time">
+                        {event.start.dateTime.slice(0,10)}                  
+                    </p>
+                    
+                    <p className="location">{event.location}</p>  
+
+                </div>
+
                 <button 
                     className="btn-toggle-details" 
                     onClick={this.handleClick}>
