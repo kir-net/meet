5e6b1da7011d88e12b4ca1093fac038ba584a64b
Kirsten <kirsten.netter@gmx.de>
2022-07-18 11:07:22 +0200

    added Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('e02a89443d7e47f6b95b80f330511294').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,3 +23,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
